21aaf6bdef91e3afb4b81fdac4adcda63be4b218
Larry Masinter <lmm@acm.org>
2023-11-29 12:46:13 -0800

    update

---
+++ bin/merge-open-prs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -x
+
+# Fetch the name of the current branch
+cb=$(git branch --show-current)
+
+    # List all open pull requests
+op=$(gh pr list --state open --json number -q '.[].number')
+
+# Loop over each open PR
+for pr in $op
+do
+  # Checkout to the PR branch
+  gh pr checkout $pr || exit 1
+  git pull || exit 1
+  # Switch back to the original branch
+  git checkout $cb
+
+  # Merge the PR branch
+  git merge --no-ff --no-edit FETCH_HEAD || exit 1
+done
+
+# Push the changes to the remote repository
+git push origin $cb
